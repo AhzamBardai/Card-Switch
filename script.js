6d60c5752fcd590e0d6421c49df60f7b2206fafc
aahzambardai <ahzam.bardai@outlook.com>
2021-08-04 19:02:58 -0500

    all styling done, MVP finished

--- script.js
+++ script.js
@@ -1,8 +1,33 @@
+let pokeArr = []
+for(i=1; i<20 ; i++){
+
+    rand = Math.floor(Math.random() * 899)
+    const url = `https://pokeapi.co/api/v2/pokemon/${rand}`
+
+    fetch(url)
+        .then(res => {
+            return res.json()
+        })
+        .then(res => {
+            
+            if(res.sprites.other.dream_world.front_default){
+                pokeArr.push(res.sprites.other.dream_world.front_default)
+            } else if(res.sprites.other['official-artwork'].front_default){
+                pokeArr.push(res.sprites.other['official-artwork'].front_default)
+            } 
+
+        })
+        .catch(error => {
+            console.log("Error!", error)
+        })  
+}
+
+
 const cards = [['😄', '😉', '🐭', '🖱️', '🧠', '🙃', '😠','🤑', '😴', '😷', '🤓', '😑', '🤯', '😤', '🤡', '💀', '👽', '👏', '😡', '💯', '🖤', '💣', '💅', '💻'],
-               ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'],
-               ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13']
-               
+               ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']               
 ]
+
+cards.push(pokeArr)
 
 const easyBtn = document.querySelector('.easy')
 const medBtn = document.querySelector('.medium')
@@ -13,12 +38,20 @@ const endModal = document.querySelector('#win-modal')
 const board = document.querySelector('.score-board')
 const score = document.querySelector('.score')
 const clear = document.querySelector('.clear')
-
 const right = document.querySelector('.right')
 const wrong = document.querySelector('.wrong')
 const allCards = document.querySelector('.cards')
 const start = document.querySelector('.first-page')
 const winner = document.querySelector('.win-screen')
+
+const settingsBtn = document.querySelector('.open-settings')
+let img = document.createElement('img')
+img.setAttribute('src', 'images/settings.png')
+img.setAttribute('id', 'settings-image')
+settingsBtn.appendChild(img)
+const settings = document.querySelector('.side-modal')
+
+
 
 let first,
     second,
@@ -40,11 +73,6 @@ easyBtn.addEventListener('click', function(e){
     allCards.style.gridTemplateColumns = 'repeat(4, 50px )'
     allCards.style.gridRowGap = '4rem'
     allCards.style.gridColumnGap = '11rem'
-    startModal.style.display = 'none'
-    board.style.display = 'grid'
-    document.body.style.background = 'no-repeat center/80% url("background-img.png")'
-    document.body.style.backgroundPositionX = '12rem'
-    document.body.style.backgroundPositionY = '-5rem'
     gameMode = 1
     makeCardGrid(6)
     
@@ -54,11 +82,6 @@ medBtn.addEventListener('click', function(e){
     allCards.style.gridTemplateColumns = 'repeat(6, 50px )'
     allCards.style.gridRowGap = '3rem'
     allCards.style.gridColumnGap = '7rem'
-    startModal.style.display = 'none'
-    board.style.display = 'grid'
-    document.body.style.background = 'no-repeat center/80% url("background-img.png")'
-    document.body.style.backgroundPositionX = '12rem'
-    document.body.style.backgroundPositionY = '-5rem'
     gameMode = 2
     makeCardGrid(9)
     
@@ -67,11 +90,6 @@ hardBtn.addEventListener('click', function(e){
     e.preventDefault()
     allCards.style.gridTemplateColumns = 'repeat(6, 50px )'
     allCards.style.gridColumnGap = '7rem'
-    startModal.style.display = 'none'
-    board.style.display = 'grid'
-    document.body.style.background = 'no-repeat center/80% url("background-img.png")'
-    document.body.style.backgroundPositionX = '12rem'
-    document.body.style.backgroundPositionY = '-5rem'
     gameMode = 3
     makeCardGrid(12)
     
@@ -86,7 +104,11 @@ function makeCardGrid(num) {
         score.innerText = 0
     }
 
-    console.log(localStorage.getItem('keepScore'))
+
+    document.body.style.background = `no-repeat center/100% url(backgrounds/boxes.jpg)`
+    startModal.style.display = 'none'
+    board.style.display = 'grid'
+    clear.style.display = 'block'
 
     randArr = Math.floor(Math.random() * cards.length)
     randIndex = Math.floor(Math.random() * (cards[randArr].length - num))
@@ -99,19 +121,28 @@ function makeCardGrid(num) {
             newCard = document.createElement('div')
             front = document.createElement('div')
             back = document.createElement('div')
-            backImage = document.createElement('div')
 
             newCard.classList.add('card')
             newCard.setAttribute('data-cardnum', i)
             front.classList.add('card-front')
             back.classList.add('card-back')
-            backImage.classList.add('back-image')
-            backImage.textContent = cards[randArr][i]
+            if(randArr === 2){
+                pokeImage = document.createElement('img')
+                pokeImage.setAttribute('src', cards[randArr][i] )
+                pokeImage.setAttribute('id', 'poke-image')
+                back.appendChild(pokeImage)
+                front.style.background = 'no-repeat center/100% url("images/pokeball.gif")';
+                front.style.right = '.8rem'
+            } else {
+                backImage = document.createElement('div')
+                backImage.classList.add('back-image')
+                backImage.textContent = cards[randArr][i]
+                back.appendChild(backImage)
+            }
 
-            if(randArr !== 0){
+            if(randArr === 1){
                 back.classList.add('card-back-words')
             }
-            back.appendChild(backImage)
             newCard.appendChild(front)
             newCard.appendChild(back)
 
@@ -204,17 +235,6 @@ window.onload = function () {
     }, 1000)
 }
 
-function finishGame() {
-    setTimeout(() => {
-        endModal.style.display = 'block'
-    }, 2000);
-    restartBtn.addEventListener('click', function() {
-        endModal.style.display = 'none'
-        startModal.style.display = 'block'
-        location.reload()
-    })
-}
-
 function scoreCount(bool) {
     if(gameMode === 1){
         if(bool){
@@ -246,7 +266,33 @@ function scoreCount(bool) {
 
 }
 
+function finishGame() {
+    setTimeout(() => {
+        endModal.style.display = 'block'
+    }, 2000);
+    setTimeout(() => {
+        document.querySelector('.play-again').innerHTML = 'Wanna play again?'
+    }, 1000)
+    restartBtn.addEventListener('click', function() {
+        endModal.style.display = 'none'
+        startModal.style.display = 'block'
+        location.reload()
+    })
+}
+
+
 clear.addEventListener('click', () => {
     score.innerText = 0
     localStorage.clear()
 })
+
+
+settingsBtn.addEventListener('click', () => {
+    
+    document.querySelector('.side-modal').style.display = 'block'
+})
+
+document.querySelector('.close').addEventListener('click', () => {
+    document.querySelector('.side-modal').style.display = 'none'
+})
+
